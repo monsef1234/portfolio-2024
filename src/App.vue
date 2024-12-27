dc27b7902e4583d89dc545083f20e3b06a2409b5
monsef1234 <monsef4566@gmail.com>
2024-12-27 06:47:40 +0100

    refactoring code

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ import { defineComponent } from "vue";
 // import Preloader from "@/components/preloader/Index.vue";
 // import Shutdown from "@/components/shutdown/Index.vue";
 // import { useVisitorsStore } from "./stores/visitors";
-import { emitter } from "./main";
+// import { emitter } from "./main";
 // import { sanityClient } from "./sanity";
 
 export default defineComponent({
@@ -55,78 +55,67 @@ export default defineComponent({
     // motions() {
     //   return useMotions();
     // },
-
     // async visitorHandler() {
     //   // this.visitorsStore.setLoading(true);
     //   try {
     //     const isVisited = localStorage.getItem("isVisited");
     //     const query = `count(*[_type == "visitor"])`;
-
     //     if (isVisited) {
     //       const visitorsCount = await sanityClient.fetch(query);
     //       // this.visitorsStore.setVisitorsCount(visitorsCount);
     //       return;
     //     }
-
     //     const data: { country_name: string; city: string } =
     //       await this.getVisitorLocation();
-
     //     await sanityClient.create({
     //       _type: "visitor",
     //       country_name: data.country_name,
     //       city: data.city,
     //     });
-
     //     const visitorsCount = await sanityClient.fetch(query);
-
     //     // this.visitorsStore.setVisitorsCount(visitorsCount);
     //     localStorage.setItem("isVisited", "true");
     //   } catch (error) {
     //     console.log(error);
     //   }
     // },
-
-    async getVisitorLocation() {
-      const response = await fetch(
-        `https://api.ipapi.com/check?access_key=${import.meta.env.VITE_KEY}`
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch location data");
-      }
-
-      return response.json();
-    },
+    // async getVisitorLocation() {
+    //   const response = await fetch(
+    //     `https://api.ipapi.com/check?access_key=${import.meta.env.VITE_KEY}`
+    //   );
+    //   if (!response.ok) {
+    //     throw new Error("Failed to fetch location data");
+    //   }
+    //   return response.json();
+    // },
   },
 
   mounted() {
-    const minDisplayTime = new Promise((resolve) => setTimeout(resolve, 3500));
-    const loadComplete = new Promise((resolve: any) => {
-      if (document.readyState === "complete") {
-        resolve();
-      } else {
-        window.addEventListener("load", resolve);
-      }
-    });
-
+    // const minDisplayTime = new Promise((resolve) => setTimeout(resolve, 3500));
+    // const loadComplete = new Promise((resolve: any) => {
+    //   if (document.readyState === "complete") {
+    //     resolve();
+    //   } else {
+    //     window.addEventListener("load", resolve);
+    //   }
+    // });
     // Promise.all([minDisplayTime, loadComplete, this.visitorHandler()]).then(
     //   () => {
     //     this.preloader = false;
     //     this.visitorsStore.setLoading(false);
     //   }
     // );
-
-    emitter.on("shutdown", () => {
-      // this.shutdown = true;
-    });
-    emitter.on("restart", () => {
-      // this.shutdown = false;
-    });
+    // emitter.on("shutdown", () => {
+    //   // this.shutdown = true;
+    // });
+    // emitter.on("restart", () => {
+    //   // this.shutdown = false;
+    // });
   },
 
-  beforeUnmount() {
-    emitter.off("shutdown");
-    emitter.off("restart");
-  },
+  // beforeUnmount() {
+  //   emitter.off("shutdown");
+  //   emitter.off("restart");
+  // },
 });
 </script>
