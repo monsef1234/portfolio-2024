3edf120d9122a4e0c4790c74d3ff0d3bc40bc423
monsef1234 <monsef4566@gmail.com>
2024-12-27 06:06:50 +0100

    chore: remive unused dependecies, docs: update desc in packages.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
-const routes = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
